17b02f0e0c6def5033bfc6892fcfee9c7f14ea3b
steynfrancois10-dev <steynfrancois10@gmail.com>
2025-11-11 21:08:03 +0200

    added  second function

--- Wildlife/management/commands/run_orm.py
+++ Wildlife/management/commands/run_orm.py
@@ -1,27 +1,39 @@
 from django.core.management.base import BaseCommand
 from Wildlife.models import Property
+from Wildlife.models import Province
+from django.db.models import Q
+
+
 
 class Command(BaseCommand):
     help = "Run Wildlife ORM"
 
     def function_1(self):
         """Get properties by type"""
-        print("1. Get properties by type")
-        # implement your code
+        print("1. Properties by type ('Private' or 'Community'):")
         properties = Property.objects.filter(
             property_type__name__in=['Private', 'Community']
-        )
+        ).order_by('name')  # optional: alphabetically
 
         if properties.exists():
-            for prop in properties:
-                print(f"- {prop.name} ({prop.property_type.name})")
+            for i, prop in enumerate(properties, start=1):
+                print(f"   {i}. {prop.name} ({prop.property_type.name})")
         else:
-            print("No properties found with type 'Private' or 'Community'.")
+            print("   No properties found.")
 
     def function_2(self):
         """Get provinces with organisations or properties"""
-        print("2. Get provinces with organisations or properties")
-        # implement your code
+        print("\n2. Provinces with organisations or properties:")
+        
+        provinces = Province.objects.filter(
+            Q(organisation__isnull=False) | Q(property__isnull=False)
+        ).distinct().order_by('name')  # optional: alphabetical
+
+        if provinces.exists():
+            for i, province in enumerate(provinces, start=1):
+                print(f"   {i}. {province.name}")
+        else:
+            print("   No provinces found.")
 
     def handle(self, *args, **options):
         """Logic of the command"""
